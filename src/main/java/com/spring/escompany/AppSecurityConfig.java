c28319f76dca8c9cd818c377f548e1d2a9bcd661
Maurizio <mauriziobella@gmail.com>
2019-11-05 22:09:15 -0500

    first webapp

---
+++ src/main/java/com/spring/escompany/AppSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.spring.escompany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	// HERE TO SAVE USERNAME AND PASSWORD IN THE DATABASE POSTGRESS
+	
+	@Autowired
+	private UserDetailsService UserDetailsService;
+	
+	@Bean
+	public AuthenticationProvider authProvider()
+	{
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(UserDetailsService);
+		
+		//password stored in the database using the BCrypt
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return provider;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http
+		.csrf().disable()
+		.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.antMatchers("/").authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login").permitAll()
+		.and()
+		.logout().invalidateHttpSession(true)
+		.clearAuthentication(true)
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/logout-success").permitAll();
+	}
+	
+	
+}
